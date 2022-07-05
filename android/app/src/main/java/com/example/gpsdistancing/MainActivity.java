d40e98e1b59f85868551e85d833d9cff0dea18a6
viktorvoltz <vctrchinyeaka@gmail.com>
2022-07-05 20:53:09 +0100

    initial commit

---
+++ android/app/src/main/java/com/example/gpsdistancing/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.gpsdistancing;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+public class MainActivity extends FlutterActivity {
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+    }
+}
