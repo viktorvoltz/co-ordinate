c0b5af831791980b17fbd2fe8cdd0a5563215962
viktorvoltz <vctrchinyeaka@gmail.com>
2022-07-06 11:53:48 +0100

    getdistance

--- android/app/src/main/java/com/example/gpsdistancing/MainActivity.java
+++ android/app/src/main/java/com/example/gpsdistancing/MainActivity.java
@@ -1,18 +1,69 @@
 package com.example.gpsdistancing;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 import io.flutter.embedding.android.FlutterActivity;
 import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
 
 public class MainActivity extends FlutterActivity {
+
+    private static final String CHANNEL = "con.chinonso.dev/gpsdistancing";
+
+    double currentLon = 0;
+    double currentLat = 0;
+    double lastLon = 0;
+    double lastLat = 0;
+    String distance;
 
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
         super.configureFlutterEngine(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
+                .setMethodCallHandler((call, result) -> {
+
+                });
+    }
+
+    private void getDistance(){
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION },
+                    1);
+            return;
+        }
+
+        lm.requestLocationUpdates(lm.GPS_PROVIDER, 0, 0, Loclist);
+        @SuppressLint("MissingPermission") Location loc = lm.getLastKnownLocation(lm.GPS_PROVIDER);
+
+        if(loc==null){
+            distance = "No location Found";
+        }
+        else{
+            //set Current latitude and longitude
+            currentLon=loc.getLongitude();
+            currentLat=loc.getLatitude();
+            display.setText("Long" + currentLon);
+
+
+        }
     }
 }
