7d12950d716f136f7db769a36fbcd36fe5cc6281
viktorvoltz <vctrchinyeaka@gmail.com>
2022-07-06 16:36:27 +0100

    get cordinate

--- lib/gpsdistancing/gps_distancing.dart
+++ lib/gpsdistancing/gps_distancing.dart
@@ -11,12 +11,37 @@ class GpsDistancing extends StatefulWidget {
 class _GpsDistancingState extends State<GpsDistancing> {
 
   static const platform = MethodChannel('com.chinonso.dev/gpsdistancing');
-  String _distance = 'Unknown distance.';
+  String _cordinate = 'Unknown distance.';
+
+  Future<void> _getCordinate() async{
+    String cordinate;
+    try {
+      final int result = await platform.invokeMethod('getCordinate');
+      cordinate = 'Cordinates: \n $result .';
+    } on PlatformException catch (e) {
+      cordinate = "Failed to get location: '${e.message}'.";
+    }
+
+    setState(() {
+      _cordinate = cordinate;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Material(
+      child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ElevatedButton(
+            child: const Text('Get current cords'),
+            onPressed: _getCordinate,
+          ),
+          Text(_cordinate),
+        ],
+      ),
+    ),
     );
   }
 }
