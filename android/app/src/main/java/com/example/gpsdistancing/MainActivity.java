60bb0275f22399f03c58707e1d6cd18ffbc77e70
viktorvoltz <vctrchinyeaka@gmail.com>
2022-08-08 17:30:23 +0100

    smol change

--- android/app/src/main/java/com/example/gpsdistancing/MainActivity.java
+++ android/app/src/main/java/com/example/gpsdistancing/MainActivity.java
@@ -49,13 +49,7 @@ public class MainActivity extends FlutterActivity {
     private String getCordinate(){
         LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+
             ActivityCompat.requestPermissions(this, new String[] {
                             Manifest.permission.ACCESS_FINE_LOCATION,
                             Manifest.permission.ACCESS_COARSE_LOCATION },
